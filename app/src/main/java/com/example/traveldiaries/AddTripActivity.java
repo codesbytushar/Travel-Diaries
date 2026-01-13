39d3d3257d69c5110e397b7637d59794cc04b280
codesbytushar <tushar.khokhar.codes@gmail.com>
2026-01-13 09:32:19 +0530

    crashing fixed!!

--- app/src/main/java/com/example/traveldiaries/AddTripActivity.java
+++ app/src/main/java/com/example/traveldiaries/AddTripActivity.java
@@ -5,11 +5,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.traveldiaries.model.Trip;
-
-import java.io.Serializable;
 import java.util.ArrayList;
 
 public class AddTripActivity extends AppCompatActivity {
@@ -28,17 +26,26 @@ public class AddTripActivity extends AppCompatActivity {
         selectPhotosBtn = findViewById(R.id.selectPhotosBtn);
         saveTripBtn = findViewById(R.id.saveTripBtn);
 
+        // Open image picker
         selectPhotosBtn.setOnClickListener(v -> {
+
             Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
             intent.setType("image/*");
             intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+
+            // REQUIRED FLAGS
+            intent.addFlags(
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION |
+                            Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION
+            );
+
             startActivityForResult(intent, 100);
         });
 
+        // Save trip
         saveTripBtn.setOnClickListener(v -> {
 
             Intent intent = new Intent();
-
             intent.putExtra("title", titleEdt.getText().toString());
             intent.putExtra("description", descEdt.getText().toString());
 
@@ -48,25 +55,43 @@ public class AddTripActivity extends AppCompatActivity {
             }
 
             intent.putStringArrayListExtra("images", imageUris);
-
             setResult(RESULT_OK, intent);
             finish();
         });
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == 100 && resultCode == RESULT_OK) {
+        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
+
             if (data.getClipData() != null) {
                 int count = data.getClipData().getItemCount();
+
                 for (int i = 0; i < count; i++) {
-                    selectedImages.add(data.getClipData().getItemAt(i).getUri());
+                    Uri uri = data.getClipData().getItemAt(i).getUri();
+
+                    // PERSIST PERMISSION (CRITICAL LINE)
+                    getContentResolver().takePersistableUriPermission(
+                            uri,
+                            Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    );
+
+                    selectedImages.add(uri);
                 }
+
             } else if (data.getData() != null) {
-                selectedImages.add(data.getData());
+
+                Uri uri = data.getData();
+
+                // PERSIST PERMISSION
+                getContentResolver().takePersistableUriPermission(
+                        uri,
+                        Intent.FLAG_GRANT_READ_URI_PERMISSION
+                );
+
+                selectedImages.add(uri);
             }
         }
     }
